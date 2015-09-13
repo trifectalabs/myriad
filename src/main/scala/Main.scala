9b6bc3fa6eb5a3be5f85c226141d3d7acb1fd11c
Josiah Witt <jdwitt@uwaterloo.ca>
2015-09-13 09:36:37 -0400

    particles talking and calculating new positions

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,11 +5,22 @@ object Main extends App {
   val system = ActorSystem("SwarmSystem")
   println("Swarm actor system started.")
 
-  println("Creating a few particles")
-  //val particleOne = ParticleImpl()
-  val particleOne = system.actorOf(Props[ParticleImpl], name = "particleOne")
-  val particleTwo = system.actorOf(Props[ParticleImpl], name = "particleTwo")
-  
-  particleOne ! UpdateGlobalBest(15.9) 
+  def obj(sol: List[Double]): Double = {
+    val x = sol.head
+    -x*x + 2.0*x + 11.0
+  }
 
+  println("Creating a few particles")
+  val r = new scala.util.Random(System.currentTimeMillis)
+  val initialSolutions = Seq.fill(8)((2.0-(-2.0))*r.nextDouble-2.0)
+  val bestSolution = initialSolutions.map(sol => obj(List(sol))).max
+  //Create particles from initial solutions
+  val particles = initialSolutions.to[Set].map(solution => 
+      system.actorOf(Props(classOf[Particle], List(0.0), 0.792, 1.4944, 1.4944, 
+      r.nextDouble, r.nextDouble, List(solution), List(bestSolution), obj _)))
+  //Using a full-connected topology
+  particles.foreach(p => p ! UpdateNeighbourhood(particles.diff(Set(p))))
+  
+  particles.foreach(p => p ! CalculateNewPosition) 
 }
+
--- src/main/scala/PSO/Particle.scala
+++ src/main/scala/PSO/Particle.scala
@@ -1,19 +1,63 @@
 import Main._
 import akka.actor._
-import WorkerMessages._
 
-abstract class Particle extends Worker {
+sealed trait ParticleMessage
+case object CalculateNewPosition extends ParticleMessage
+case class UpdateGlobalBest(best: List[Double]) extends ParticleMessage
+case class UpdateNeighbourhood(neighbours: Set[ActorRef]) extends ParticleMessage
+
+class Particle(initVelocity: List[Double], inertiaW: Double, accelCoeff1: Double, 
+    accelCoeff2: Double, randCoeff1: Double, randCoeff2: Double, lbest: List[Double], 
+    gbest: List[Double], objectiveFunction: List[Double] => Double) extends Actor {
+  val w = inertiaW
+  val c1 = accelCoeff1
+  val c2 = accelCoeff2
+  val r1 = randCoeff1
+  val r2 = randCoeff2
+  var currVel = initVelocity
+  var currPos = lbest
+  var localBest = lbest
+  var globalBest = gbest
+  var neighbourhood = Set[ActorRef]()
+  
   override def receive = {
-    case UpdateGlobalBest(v: Double) => println(s"Updating my global best value to: $v")
+    case CalculateNewPosition =>
+      println("Calculating new postion")
+      currVel = UpdateVelocity()
+      currPos = UpdatePosition()
+      if (objectiveFunction(currPos) > objectiveFunction(localBest)) {
+        println("New local best found!")
+        localBest = currPos
+      }
+      if (objectiveFunction(currPos) > objectiveFunction(globalBest)) {
+        println("New global best found!")
+        globalBest = currPos
+        neighbourhood.foreach(n => n ! UpdateGlobalBest(globalBest))
+      }
+    case UpdateGlobalBest(best: List[Double]) =>
+      println(s"Updating my global best value to: $best")
+      globalBest = best
+    case UpdateNeighbourhood(neighbours: Set[ActorRef]) => 
+      println("Updating neighbours")
+      neighbourhood = neighbours
     case _ => 
       println("do shit when they talk")
   }
 
-  override val solution = List[Double]()
-}
+  def UpdateVelocity(): List[Double] = {
+    currVel.zip(currPos).zip(localBest.zip(globalBest)).map { 
+      case ((v, p), (l, g)) =>
+        (w * v) + (c1 * r1 * (l - p)) + (c2 * r2 * (g - p))
+      case _ => throw new RuntimeException()
+    }
+  }
 
-//Class defining a single particle in the particle swarm optimization algorithm (PSO)
+  def UpdatePosition(): List[Double] = {
+    currVel.zip(currPos).map {
+      case (v, p) => p + v
+      case _ => throw new RuntimeException()
+    }
+  }
 
-class ParticleImpl extends Particle {
 }
 
